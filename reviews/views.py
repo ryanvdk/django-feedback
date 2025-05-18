3960a78dd1d2a74654687842d02159ac6022e52e
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-17 21:24:37 -0700

    Basic form implementation.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def review(request):
+    if request.method == "POST":
+        input_username = request.POST["username"]
+        if input_username == "":
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
+
+        print(input_username)
+        return HttpResponseRedirect("/thank-you")
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
