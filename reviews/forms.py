b65db5bf7333f6af584e21b9fac121ea505e4a6b
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-18 11:05:03 -0700

    Added handling for multiple fields.

--- reviews/forms.py
+++ reviews/forms.py
@@ -8,3 +8,8 @@ class ReviewForm(forms.Form):
         "required": "Your Name must not be blank.",
         "max_length": "Please enter a shorter name."
     })
+
+    review_text = forms.CharField(
+        label="Your feedback", widget=forms.Textarea, max_length=200)
+
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
