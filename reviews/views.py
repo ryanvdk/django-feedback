ab969981bc41bb0f369f05aa3bb6930d319a2284
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-18 11:42:58 -0700

    Converted the Review view to a class based view.

--- reviews/views.py
+++ reviews/views.py
@@ -1,27 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
 from .models import Review
 # Create your views here.
 
 
-def review(request):
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data["user_name"], review_text=form.cleaned_data["review_text"], rating=form.cleaned_data["rating"])
-            review.save()
-            return HttpResponseRedirect("/thank-you")
-
-    else:
+class ReviewView(View):
+    def get(self, request):
         form = ReviewForm()
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+
+# def review(request):
+#     if request.method == "POST":
+
+#         # existing_data = Review.objects.get(pk=1)
+#         # form = ReviewForm(request.POST, instance=existing_data)
+
+#         form = ReviewForm(request.POST)
+
+#         if form.is_valid():
+#             # review = Review(
+#             #     user_name=form.cleaned_data["user_name"], review_text=form.cleaned_data["review_text"], rating=form.cleaned_data["rating"])
+#             # review.save()
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
 
 
 def thank_you(request):
