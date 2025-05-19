d1d0ce76f5afb352ad65183cc04c35b5a442bb14
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-19 11:23:55 -0700

    Setup FormView, CreateView, ListView, and DetailView. Completed Section 11.

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.ReviewView.as_view(), name="review"),
     path("thank-you", views.ThankYouView.as_view(), name="thank-you"),
     path("reviews/", views.ReviewListView.as_view(), name="review-list"),
-    path("reviews/<int:id>",
+    path("reviews/<int:pk>",
          views.ReviewDetailsView.as_view(), name="review-details")
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,27 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
 from .forms import ReviewForm
 from .models import Review
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(CreateView):
+    model = Review
+    # Alternatively to declaring the fields, you can declare a form_class and point to the custom created form.
+    # form_class = ReviewForm
+    # fields = "__all__"
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+#     success_url = "/thank-you"
+
+#     # Override the form_valid form to manipulate validated data.
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
+
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
 
 class ThankYouView(TemplateView):
@@ -35,26 +57,49 @@ class ThankYouView(TemplateView):
         return context
 
 
-class ReviewListView(TemplateView):
+class ReviewListView(ListView):
     template_name = "reviews/review_list.html"
+    model = Review
+    # Without this the data is exposed to the template as object_list
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    # Can still add methods to get more control.
+
+    # get_queryset allows you to filter the data to display
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gte=4)
+    #     return data
+
+# class ReviewListView(TemplateView):
+#     template_name = "reviews/review_list.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
 
 
-class ReviewDetailsView(TemplateView):
+class ReviewDetailsView(DetailView):
+    # This View type uses the primary key to identify which item to access. "reviews/<int:pk>"
+    # Data is accessible in the tamplate through the model name or the "object" keyword
     template_name = "reviews/review_details.html"
+    model = Review
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        id = self.kwargs.get("id")
-        review = Review.objects.get(id=id)
-        context["title"] = f"{review.user_name}'s Review"
-        context["details"] = review.review_text
-        return context
+
+# class ReviewDetailsView(TemplateView):
+#     template_name = "reviews/review_details.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         # id = self.kwargs.get("id")
+#         review_id = kwargs["id"]
+#         review = Review.objects.get(id=review_id)
+#         context["title"] = f"{review.user_name}'s Review"
+#         context["details"] = review.review_text
+#         context["rating"] = review.rating
+#         return context
 
 # class ThankYouView(View):
 #     def get(self, request):
