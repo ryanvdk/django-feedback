e6746081860dfa92153f892f66323b7ff1b54091
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-19 13:00:52 -0700

    Added Profiles app with image upload form.

---
+++ profiles/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+from .forms import ProfileForm
+from .models import UserProfile
+# Create your views here.
+
+
+def store_file(file):
+    with open("temp/" + file.name, "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        # "image" refers to the html name property given to the input element.
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            # store_file(request.FILES["image"])
+            return HttpResponseRedirect("/profiles")
+        else:
+            return render(request, "profiles/create_profile.html", {
+                "form": submitted_form
+            })
